6f88eaf7220311640f07fcca6081e5c8293f9516
unknown <gayoyawalkar@gmail.com>
2021-10-21 07:10:42 +0530

    Welcome to user registration with exception

---
+++ Hello.java
@@ -0,0 +1,6 @@
+package com.bridgelabz.userregistration;
+public class Hello {
+	public static void main(String[] args) {
+		System.out.println("Welcome to User Registration Program with Custom Exception");
+	}
+}
